ee37393e9ad17e24314e9ef89c2141fcdf42bd11
Breno-Barbosa1 <brenobarbosa117@gmail.com>
2025-08-25 10:59:22 -0300

    Dockerizando a Api Restful

--- rest-with-springboot-erudio/src/main/java/br/com/breno_barbosa1/repository/BookRepository.java
+++ rest-with-springboot-erudio/src/main/java/br/com/breno_barbosa1/repository/BookRepository.java
@@ -1,11 +1,9 @@
 package br.com.breno_barbosa1.repository;
 
 import br.com.breno_barbosa1.model.Book;
-import br.com.breno_barbosa1.model.Person;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
